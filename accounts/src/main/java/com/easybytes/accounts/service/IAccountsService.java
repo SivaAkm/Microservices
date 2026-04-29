e2d7bd1ac269f3efecfc7e5c7d380d63beaab5a0
SivaAkm <gsivaakm2001@gmail.com>
2026-04-29 22:33:55 +0530

    crud operations

--- accounts/src/main/java/com/easybytes/accounts/service/IAccountsService.java
+++ accounts/src/main/java/com/easybytes/accounts/service/IAccountsService.java
@@ -15,4 +15,17 @@ public interface IAccountsService {
      * @return Account details based on mobile number
      */
     CustomerDto fetchAccount(String mobileNumber);
+
+/**
+ *@param customerDto - CustomerDto Object
+ * @return boolean indicating if the update of Account details is successful or not
+ */
+boolean updateAccount(CustomerDto customerDto);
+
+    /**
+     * *
+     * @param mobileNumber - Input Mobile Number
+     * @return boolean indicating if the delete of Account details is successful or not
+     */
+    boolean deleteAccount(String mobileNumber);
 }
